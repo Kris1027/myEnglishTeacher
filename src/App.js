0a74bae5a3d9652b1860213d2785a97ad1d4decc
Kris1027 <obarzanek.work@gmail.com>
2023-08-04 19:10:23 +0200

    add handleChangeCard function and polish side class

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
+import { useState } from 'react';
+
 const words = [
   {
+    id: 1,
     english: 'Essential',
     polish: 'Niezbędny',
   },
   {
+    id: 2,
     english: 'Versatile',
     polish: 'Wszechstronny',
   },
   {
+    id: 3,
     english: 'Executed',
     polish: 'Wykonany',
   },
   {
+    id: 4,
     english: 'Accumulate',
     polish: 'Gromadzić',
   },
   {
+    id: 5,
     english: 'Boil to',
     polish: 'Sprowadzić',
   },
 ];
 
 export default function App() {
+  const [changeCard, setChangeCard] = useState('');
+
+  function handleChangeCard(id) {
+    setChangeCard(id !== changeCard ? id : null);
+  }
+
   return (
     <>
       <header>
@@ -29,8 +42,14 @@ export default function App() {
       </header>
       <main className='words-wrapper'>
         {words.map((word) => (
-          <div className='words-card english-side' key={word.english}>
-            {word.english}
+          <div
+            onClick={() => handleChangeCard(word.id)}
+            className={`words-card ${
+              word.id === changeCard ? 'polish-side' : 'english-side'
+            }`}
+            key={word.id}
+          >
+            {word.id === changeCard ? word.polish : word.english}
           </div>
         ))}
       </main>

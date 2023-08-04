01a995d2e3b2a9c31e8669f3e4971d07b96f0936
Kris1027 <obarzanek.work@gmail.com>
2023-08-04 19:45:47 +0200

    add button without functionality

--- src/App.js
+++ src/App.js
@@ -230,6 +230,13 @@ export default function App() {
       <header>
         <h1 className='heading'>kris personal english words teacher</h1>
       </header>
+      <nav className='menu'>
+        <button>1-20</button>
+        <button>21-40</button>
+        <button>41-60</button>
+        <button>61-80</button>
+        <button>81-100</button>
+      </nav>
       <main className='words-wrapper'>
         {words.map((word) => (
           <div
